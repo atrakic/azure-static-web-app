7f30bf5c340a16bb3168487a791ac1e4a71e2a61
Admir Trakic <atrakic@users.noreply.github.com>
2025-08-16 10:15:06 +0200

    Update healthcheck.js

--- src/functions/healthcheck.js
+++ src/functions/healthcheck.js
@@ -1,11 +1,16 @@
 const { app } = require('@azure/functions');
 
 app.http('healthcheck', {
-    methods: ['GET'],
+    methods: ['GET', 'POST'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
+        const name = req.query.name || (req.body && req.body.name);
         const currentTime = new Date().toISOString();
-        return { body: JSON.stringify({ "text": currentTime }) };
+        return { body: JSON.stringify({
+            "message": `Hello, ${name || 'World'}!`,
+            "status": "healthy",
+            "time": currentTime
+         }) };
     }
 });
